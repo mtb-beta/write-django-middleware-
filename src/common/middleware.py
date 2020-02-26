f0750aba2602331ec0e1ac557fdb990866433a6c
mtb-beta <mtb.toya0403@gmail.com>
2020-02-26 10:45:25 +0900

    django version up 1.11.27

--- src/common/middleware.py
+++ src/common/middleware.py
@@ -1,4 +1,5 @@
-class MyLoggingMiddleware(object):
+class MyLoggingMiddleware:
+
     def __init__(self, get_responce):
         self.get_responce = get_responce
         print("MyLogging Middleware init")
